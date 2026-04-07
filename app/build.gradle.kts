5e9f6cadbb9ef5384015eaec6c0e2203e8a8c29d
ThanhThankC <156212398+ThanhThankC@users.noreply.github.com>
2026-04-07 22:23:59 +0700

    chore: update config of Android Studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.google.code.gson:gson:2.14.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
